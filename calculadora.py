a1e85aa9785974067dc874ac3309f4db6b98ee29
LuisHernandez1999 <105570863+LuisHernandez1999@users.noreply.github.com>
2022-08-05 07:44:50 -0300

    calculadora

--- calculadora.py
+++ calculadora.py
@@ -3,16 +3,27 @@ while True:
     num_1 = input( 'Digite um número: ')
     num_2 = input('Digite outro número: ')
     operador = input ('Digite um operador: ')
+    sair = input('Deseja sair ? [s]im ou [n]ão: ')
+    
+    if sair == 's':
+        break 
     
     if not num_1.isnumeric() or num_2.isnumeric():
-        
-        num_1 = int(num_1)
-        num_2 = int(num_2)
+        print('Você precisa entrar um número ')
+        continue 
+    
+        num_1 = float(num_1)
+        num_2 = float(num_2)
         
     if operador == '+':
          print(num_1 + num_2)
-         
     elif operador == "-":
-        print(num_1 + num_2)
+        print(num_1 - num_2)
+    elif operador == '/':
+        print(num_1 / num_2)
+    elif operador == '*':
+        print(num_1 * num_2 )  
+    else:
+        print('Operador inválido')            
     
             

ad4811cb44eff2b1712e299930c247b524c3eac1
LuisHernandez1999 <105570863+LuisHernandez1999@users.noreply.github.com>
2022-08-05 09:26:39 -0300

    contadores

---
+++ Contadores.py
@@ -0,0 +1,13 @@
+contador = 1 
+acumulador = 1 
+
+while contador <= 100: 
+    print (contador, acumulador)
+    
+    
+    
+    acumulador = acumulador + contador 
+    contador += 1  
+else: 
+    print('Cheguei no else ')    
+print('Isso será executado ')    
